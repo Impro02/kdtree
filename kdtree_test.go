9799ced1ed5418a6e6d5f83ce48da7e6c9e396d2
Maxime MARTIN <maxime.martin@st.com>
2024-02-26 23:13:09 +0100

    feat: distance is square euclidean

--- kdtree_test.go
+++ kdtree_test.go
@@ -1,33 +1,31 @@
 package kdtree
 
 import (
-	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var points = []Point{
+	&PointBase{Vec: []float64{6, 6}},
+	&PointBase{Vec: []float64{1, 1}},
+	&PointBase{Vec: []float64{2, 2}},
+	&PointBase{Vec: []float64{10, 10}},
+	&PointBase{Vec: []float64{43, 43}},
+	&PointBase{Vec: []float64{9, 9}},
+	&PointBase{Vec: []float64{21, 21}},
+	&PointBase{Vec: []float64{3, 3}},
+	&PointBase{Vec: []float64{22, 22}},
+	&PointBase{Vec: []float64{40, 40}},
+	&PointBase{Vec: []float64{41, 41}},
+	&PointBase{Vec: []float64{20, 20}},
+	&PointBase{Vec: []float64{42, 42}},
+	&PointBase{Vec: []float64{100, 100}},
+}
+
+var kdTree = BuildKDTree(points, 0)
+
 func TestKNearestNeighbors(t *testing.T) {
-
-	points := []Point{
-		&PointBase{Vec: []float64{6, 6}},
-		&PointBase{Vec: []float64{1, 1}},
-		&PointBase{Vec: []float64{2, 2}},
-		&PointBase{Vec: []float64{10, 10}},
-		&PointBase{Vec: []float64{43, 43}},
-		&PointBase{Vec: []float64{9, 9}},
-		&PointBase{Vec: []float64{21, 21}},
-		&PointBase{Vec: []float64{3, 3}},
-		&PointBase{Vec: []float64{22, 22}},
-		&PointBase{Vec: []float64{40, 40}},
-		&PointBase{Vec: []float64{41, 41}},
-		&PointBase{Vec: []float64{20, 20}},
-		&PointBase{Vec: []float64{42, 42}},
-		&PointBase{Vec: []float64{100, 100}},
-	}
-
-	kdTree := BuildKDTree(points, 0)
 
 	target := &PointBase{Vec: []float64{3, 3}}
 	k := 5
@@ -62,27 +60,8 @@ func TestKNearestNeighbors(t *testing.T) {
 
 func TestNeighborsWithinRadius(t *testing.T) {
 
-	points := []Point{
-		&PointBase{Vec: []float64{6, 6}},
-		&PointBase{Vec: []float64{1, 1}},
-		&PointBase{Vec: []float64{2, 2}},
-		&PointBase{Vec: []float64{10, 10}},
-		&PointBase{Vec: []float64{43, 43}},
-		&PointBase{Vec: []float64{9, 9}},
-		&PointBase{Vec: []float64{21, 21}},
-		&PointBase{Vec: []float64{3, 3}},
-		&PointBase{Vec: []float64{22, 22}},
-		&PointBase{Vec: []float64{40, 40}},
-		&PointBase{Vec: []float64{41, 41}},
-		&PointBase{Vec: []float64{20, 20}},
-		&PointBase{Vec: []float64{42, 42}},
-		&PointBase{Vec: []float64{100, 100}},
-	}
-
-	kdTree := BuildKDTree(points, 0)
-
 	target := &PointBase{Vec: []float64{3, 3}}
-	k := 5.0
+	k := 5.0 * 5.0
 	workers := 2
 
 	neighbors := kdTree.NeighborsWithinRadius(target, k, workers)
@@ -108,28 +87,4 @@ func TestNeighborsWithinRadius(t *testing.T) {
 	}
 
 	assert.Equal(t, expectedPoint, neighbors)
-}
-
-func TestNeighborsWithinRadiusLargeDataset(t *testing.T) {
-	// Generate a large dataset
-	points := make([]Point, 1000000)
-	for i := range points {
-		points[i] = &PointBase{Vec: []float64{rand.Float64() * 1000, rand.Float64() * 1000}}
-	}
-
-	// Create a KDTree with the points
-	kdTree := BuildKDTree(points, 0)
-
-	// Test NeighborsWithinRadius
-	center := &PointBase{Vec: []float64{500, 500}}
-	radius := 100.0
-	start := time.Now()
-	neighbors := kdTree.NeighborsWithinRadius(center, radius, 2)
-	elapsed := time.Since(start)
-	t.Logf("Found %d neighbors within radius %.2f of point %.2f in %s", len(neighbors), radius, center.Vector(), elapsed)
-
-	// Add assertions as needed, for example:
-	if len(neighbors) == 0 {
-		t.Errorf("Expected to find neighbors, but found none")
-	}
 }
