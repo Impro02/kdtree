f91ab69ce23c6b3e214eed2d4dd95e13234d2e98
Maxime MARTIN <maxime.martin@st.com>
2024-02-27 11:46:32 +0100

    feat: add tests on large dataset

--- kdtree_test.go
+++ kdtree_test.go
@@ -1,31 +1,32 @@
 package kdtree
 
 import (
+	"math"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-var points = []Point{
-	&PointBase{Vec: []float64{6, 6}},
-	&PointBase{Vec: []float64{1, 1}},
-	&PointBase{Vec: []float64{2, 2}},
-	&PointBase{Vec: []float64{10, 10}},
-	&PointBase{Vec: []float64{43, 43}},
-	&PointBase{Vec: []float64{9, 9}},
-	&PointBase{Vec: []float64{21, 21}},
-	&PointBase{Vec: []float64{3, 3}},
-	&PointBase{Vec: []float64{22, 22}},
-	&PointBase{Vec: []float64{40, 40}},
-	&PointBase{Vec: []float64{41, 41}},
-	&PointBase{Vec: []float64{20, 20}},
-	&PointBase{Vec: []float64{42, 42}},
-	&PointBase{Vec: []float64{100, 100}},
-}
-
-var kdTree = BuildKDTree(points, 0)
-
 func TestKNearestNeighbors(t *testing.T) {
+	points := []Point{
+		&PointBase{Vec: []float64{6, 6}},
+		&PointBase{Vec: []float64{1, 1}},
+		&PointBase{Vec: []float64{2, 2}},
+		&PointBase{Vec: []float64{10, 10}},
+		&PointBase{Vec: []float64{43, 43}},
+		&PointBase{Vec: []float64{9, 9}},
+		&PointBase{Vec: []float64{21, 21}},
+		&PointBase{Vec: []float64{3, 3}},
+		&PointBase{Vec: []float64{22, 22}},
+		&PointBase{Vec: []float64{40, 40}},
+		&PointBase{Vec: []float64{41, 41}},
+		&PointBase{Vec: []float64{20, 20}},
+		&PointBase{Vec: []float64{42, 42}},
+		&PointBase{Vec: []float64{100, 100}},
+	}
+
+	kdTree := BuildKDTree(points, 0)
 
 	target := &PointBase{Vec: []float64{3, 3}}
 	k := 5
@@ -34,11 +35,11 @@ func TestKNearestNeighbors(t *testing.T) {
 	neighbors := kdTree.KNearestNeighbors(target, k, workers)
 
 	expectedPoint := []Point{
-		&PointBase{Vec: []float64{3, 3}},
-		&PointBase{Vec: []float64{2, 2}},
-		&PointBase{Vec: []float64{1, 1}},
-		&PointBase{Vec: []float64{6, 6}},
 		&PointBase{Vec: []float64{9, 9}},
+		&PointBase{Vec: []float64{6, 6}},
+		&PointBase{Vec: []float64{1, 1}},
+		&PointBase{Vec: []float64{2, 2}},
+		&PointBase{Vec: []float64{3, 3}},
 	}
 
 	assert.Equal(t, expectedPoint, neighbors)
@@ -48,17 +49,35 @@ func TestKNearestNeighbors(t *testing.T) {
 	neighbors = kdTree.KNearestNeighbors(target, k, workers)
 
 	expectedPoint = []Point{
-		&PointBase{Vec: []float64{42, 42}},
+		&PointBase{Vec: []float64{22, 22}},
+		&PointBase{Vec: []float64{40, 40}},
 		&PointBase{Vec: []float64{41, 41}},
 		&PointBase{Vec: []float64{43, 43}},
-		&PointBase{Vec: []float64{40, 40}},
-		&PointBase{Vec: []float64{22, 22}},
+		&PointBase{Vec: []float64{42, 42}},
 	}
 
 	assert.Equal(t, expectedPoint, neighbors)
 }
 
 func TestNeighborsWithinRadius(t *testing.T) {
+	points := []Point{
+		&PointBase{Vec: []float64{6, 6}},
+		&PointBase{Vec: []float64{1, 1}},
+		&PointBase{Vec: []float64{2, 2}},
+		&PointBase{Vec: []float64{10, 10}},
+		&PointBase{Vec: []float64{43, 43}},
+		&PointBase{Vec: []float64{9, 9}},
+		&PointBase{Vec: []float64{21, 21}},
+		&PointBase{Vec: []float64{3, 3}},
+		&PointBase{Vec: []float64{22, 22}},
+		&PointBase{Vec: []float64{40, 40}},
+		&PointBase{Vec: []float64{41, 41}},
+		&PointBase{Vec: []float64{20, 20}},
+		&PointBase{Vec: []float64{42, 42}},
+		&PointBase{Vec: []float64{100, 100}},
+	}
+
+	kdTree := BuildKDTree(points, 0)
 
 	target := &PointBase{Vec: []float64{3, 3}}
 	k := 5.0 * 5.0
@@ -67,10 +86,10 @@ func TestNeighborsWithinRadius(t *testing.T) {
 	neighbors := kdTree.NeighborsWithinRadius(target, k, workers)
 
 	expectedPoint := []Point{
-		&PointBase{Vec: []float64{3, 3}},
+		&PointBase{Vec: []float64{6, 6}},
 		&PointBase{Vec: []float64{2, 2}},
 		&PointBase{Vec: []float64{1, 1}},
-		&PointBase{Vec: []float64{6, 6}},
+		&PointBase{Vec: []float64{3, 3}},
 	}
 
 	assert.Equal(t, expectedPoint, neighbors)
@@ -81,10 +100,106 @@ func TestNeighborsWithinRadius(t *testing.T) {
 
 	expectedPoint = []Point{
 		&PointBase{Vec: []float64{42, 42}},
+		&PointBase{Vec: []float64{40, 40}},
 		&PointBase{Vec: []float64{41, 41}},
 		&PointBase{Vec: []float64{43, 43}},
-		&PointBase{Vec: []float64{40, 40}},
 	}
 
 	assert.Equal(t, expectedPoint, neighbors)
+}
+
+func TestKNearestNeighborsLargeDataset(t *testing.T) {
+	// Seed the random number generator with a constant value.
+	r := rand.New(rand.NewSource(42))
+
+	// Define the centroids.
+	centroids := []Point{
+		&PointBase{Vec: []float64{0.1, 0.1}},
+		&PointBase{Vec: []float64{0.1, 0.9}},
+		&PointBase{Vec: []float64{0.5, 0.5}},
+		&PointBase{Vec: []float64{0.9, 0.1}},
+		&PointBase{Vec: []float64{0.9, 0.9}},
+	}
+
+	points := []Point{}
+	for i := 0; i < 1000000; i++ {
+		centroid := centroids[r.Intn(len(centroids))]
+		x := centroid.GetValue(0) + r.NormFloat64()*0.1
+		y := centroid.GetValue(1) + r.NormFloat64()*0.1
+		points = append(points, &PointBase{Vec: []float64{x, y}})
+	}
+
+	kdTree := BuildKDTree(points, 0)
+
+	target := &PointBase{Vec: []float64{r.Float64(), r.Float64()}}
+	k := 5
+	workers := 2
+
+	neighbors := kdTree.KNearestNeighbors(target, k, workers)
+
+	expectedPoint := []Point{
+		&PointBase{Vec: []float64{0.3856857299481051, 0.5419476199994974}},
+		&PointBase{Vec: []float64{0.38542887388874814, 0.5417771692037563}},
+		&PointBase{Vec: []float64{0.3854901841318027, 0.541756239560265}},
+		&PointBase{Vec: []float64{0.38549966558989135, 0.5415744489834114}},
+		&PointBase{Vec: []float64{0.38564315658887993, 0.5414216231963146}},
+	}
+
+	assert.Equal(t, expectedPoint, neighbors)
+
+	target = &PointBase{Vec: []float64{r.Float64(), r.Float64()}}
+
+	neighbors = kdTree.KNearestNeighbors(target, k, workers)
+
+	expectedPoint = []Point{
+		&PointBase{Vec: []float64{0.7497493291422486, 0.3950725887601213}},
+		&PointBase{Vec: []float64{0.7466782147046156, 0.40251297114855955}},
+		&PointBase{Vec: []float64{0.7477548717536625, 0.4024430684518462}},
+		&PointBase{Vec: []float64{0.750271368071364, 0.40175926458750155}},
+		&PointBase{Vec: []float64{0.7490513219372369, 0.39989061448023705}},
+	}
+
+	assert.Equal(t, expectedPoint, neighbors)
+}
+
+func TestNeighborsWithinRadiusLargeDataset(t *testing.T) {
+	// Seed the random number generator with a constant value.
+	r := rand.New(rand.NewSource(42))
+
+	// Define the centroids.
+	centroids := []Point{
+		&PointBase{Vec: []float64{0.1, 0.1}},
+		&PointBase{Vec: []float64{0.1, 0.9}},
+		&PointBase{Vec: []float64{0.5, 0.5}},
+		&PointBase{Vec: []float64{0.9, 0.1}},
+		&PointBase{Vec: []float64{0.9, 0.9}},
+	}
+
+	points := []Point{}
+	for i := 0; i < 1000000; i++ {
+		centroid := centroids[r.Intn(len(centroids))]
+		x := centroid.GetValue(0) + r.NormFloat64()*0.1
+		y := centroid.GetValue(1) + r.NormFloat64()*0.1
+		points = append(points, &PointBase{Vec: []float64{x, y}})
+	}
+
+	kdTree := BuildKDTree(points, 0)
+
+	target := &PointBase{Vec: []float64{r.Float64(), r.Float64()}}
+	k := math.Pow(0.03, 2)
+	workers := 2
+
+	neighbors := kdTree.NeighborsWithinRadius(target, k, workers)
+
+	expectedPointCount := 38
+
+	assert.Equal(t, expectedPointCount, len(neighbors))
+
+	target = &PointBase{Vec: []float64{r.Float64(), r.Float64()}}
+
+	neighbors = kdTree.NeighborsWithinRadius(target, k, workers)
+
+	expectedPointCount = 11
+
+	assert.Equal(t, expectedPointCount, len(neighbors))
 }
